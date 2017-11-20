a660016863173f859926206e4250fcab1d4e707f
oliver <urnen81@yahoo.es>
2017-11-20 23:30:48 +0100

    test: Retrieve the settings

--- kodi/plugin.video.tdtiberia/addon.py
+++ kodi/plugin.video.tdtiberia/addon.py
@@ -7,6 +7,11 @@ import channelproviders
 addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
+
+thisAddon = xbmcaddon.Addon()
+iconsWanted = thisAddon.getSetting('retrieve_icons') # returns the string 'true' or 'false'
+# thisAddon.setSetting('my_setting', 'false')
+
 # provider = channelproviders.TvOnlineAPP()
 provider = channelproviders.GitHubMD()
 
