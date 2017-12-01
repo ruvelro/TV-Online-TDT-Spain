070228aa4e1d8ba064a76040e62ea8f696e83da0
urnenfeld <urn@kodistation.home>
2017-12-01 20:41:29 +0100

    Use JSON file Fix non enabled channels setting First tryout of setting an icon

--- kodi/plugin.video.tdtiberia/addon.py
+++ kodi/plugin.video.tdtiberia/addon.py
@@ -1,9 +1,11 @@
 import sys
+import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
 
 import channelproviders
+import icons
 
 addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
@@ -11,18 +13,26 @@ xbmcplugin.setContent(addon_handle, 'movies')
 
 thisAddon = xbmcaddon.Addon()
 iconsWanted = thisAddon.getSetting('retrieve_icons')
-includeDisabled = thisAddon.getSetting('include_disabled_channels')
-# thisAddon.setSetting('my_setting', 'false')
+includeDisabledString = thisAddon.getSetting('include_disabled_channels')
 
+if includeDisabledString == 'false':
+	includeDisabled = False
+else:
+	includeDisabled = True
 
-# provider = channelproviders.GitHubJSON(includeDisabled)
+provider = channelproviders.GitHubJSON(includeDisabled)
 # provider = channelproviders.TvOnlineAPP()
-provider = channelproviders.GitHubMD()
+# provider = channelproviders.GitHubMD()
 
 channelList = provider.retrieveList()
 
 for channel in channelList:
-	channelListItem = xbmcgui.ListItem(channel[0])
+	iconfile = icons.getIcon(channel[0])
+
+	if iconfile == None:
+		channelListItem = xbmcgui.ListItem(channel[0])
+	else:
+		channelListItem = xbmcgui.ListItem(channel[0], iconImage="resources/icons/"+iconfile)
 	xbmcplugin.addDirectoryItem(handle=addon_handle, url=channel[1], listitem=channelListItem)
 
 xbmcplugin.endOfDirectory(addon_handle)
